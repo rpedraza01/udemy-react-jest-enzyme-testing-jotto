e1fd472ca33391837da94f8b6b1f131cdc28b103
Rodrigo Pedraza <rpedraza01@gmail.com>
2023-02-08 14:16:13 -0800

    added console warns and mock warns

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -21,6 +21,7 @@ const languageStrings = {
 
 function getStringByLanguage(languageCode, stringKey, strings=languageStrings) {
     if (!strings[languageCode] || !strings[languageCode][stringKey]) {
+        console.warn(`Could not get string [${stringKey}] for [${languageCode}]`)
         return strings.en[stringKey];
     }
     return strings[languageCode][stringKey];
